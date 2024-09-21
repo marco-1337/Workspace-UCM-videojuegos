4825d223fd8e5a802bbf20722eb3315cf92a75e8
Marco <marcgo19@ucm.es>
2024-09-21 22:15:23 +0200

    cosas varias

--- PVLI/primera_web_MarcoGonzalez/script.js
+++ PVLI/primera_web_MarcoGonzalez/script.js
@@ -1,10 +1,32 @@
-function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
+function desaparece(nombre) {
+	let items = document.getElementsByClassName(nombre);
+  console.log(items);
+  for(let i = 0; i < items.length; i++)
+  {
+    console.log(items[i].style.visibility)
+    if(items[i].style.visibility == 'hidden')
+    {
+      items[i].style.visibility = 'visible';
+    }
+    else
+    {
+      items[i].style.visibility = 'hidden';
+    }
+  }
 }
 
-function desaparece(nombre) {
-	let button = document.getElementById(nombre);
-  button.style.visibility='hidden';
+function coloresCambian(nom1, nom2)
+{
+  let items1 = document.getElementsByClassName(nom1);
+  for(let i = 0; i < items1.length; i++)
+  {
+    items1[i].style.color = 'magenta';
+  }
+  let items2 = document.getElementsByClassName(nom2);
+  for(let i = 0; i < items2.length; i++)
+  {
+    items2[i].style.color = 'darkgreen';
+  }
 }
 
 function playMusic(musica){
