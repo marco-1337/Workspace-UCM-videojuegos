468989514b59c1175bc2f67051e7bca132ebfe97
Marco <marcgo19@ucm.es>
2024-10-04 18:52:09 +0200

    cosas

---
+++ EDA/EjerciciosJuez/Tema4/Tema4-01/T4-01.cpp
@@ -0,0 +1,68 @@
+// Nombre del alumno Marco Gonzalez Campo
+// Usuario del Juez EDA-GDV30
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+int sumarCuadrados(int n)
+{
+
+}
+
+// función que resuelve el problema
+void resolver(const int dato) {
+    set<int> conjunto;
+
+    int n = dato;
+
+    while(conjunto.find(n) != conjunto.end())
+    {
+        n = sumarCuadrados(n);
+    }
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    
+    int n;
+
+    cin >> n;
+
+    if (! std::cin)
+        return false;
+    
+    // escribir sol
+    resolver(n);
+    
+    
+    return true;
+    
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    
+    while (resuelveCaso());
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
